78b0b0e284e6082df2bc385b3690240880accd01
Antoine Bonneau <antoine.bonneau@edu.univ-fcomte.fr>
2020-03-03 19:16:57 +0100

    EndGame Layout

--- sources/ui/uiEndgame.lua
+++ sources/ui/uiEndgame.lua
@@ -1,13 +1,20 @@
 require('var')
-local nuklear = require 'nuklear'
+
+local widget_width  = 0.2 * WINDOW_WIDTH
+local widget_height = 0.2 * WINDOW_HEIGHT
+
+local widget_width_padding  = (WINDOW_WIDTH - widget_width)/2
+local widget_height_padding = (WINDOW_HEIGHT - widget_height)/2 
+
+local mat_font = love.graphics.newFont(15)
+local default_font = love.graphics.getFont()
 
 return function (ui, msg)
-    local mat_font = love.graphics.newFont(15)
-    local default_font = love.graphics.getFont()
     x, y, width, height = ui:windowGetContentRegion()
-    if ui:windowBegin('Fin de partie', 0, 0, WIDTH, HEIGHT) then
+    if ui:windowBegin('Fin de partie', widget_width_padding, widget_height_padding, widget_width, widget_height) then
         love.graphics.setFont(mat_font)
-        ui:label(msg, 'centered', nuklear.colorRGBA(1, 1, 1))
+        ui:layoutRow('dynamic', 30, 1) 
+        ui:label(msg, 'centered', "#FFFFFF")
 		love.graphics.setFont(default_font)
 	end
 	ui:windowEnd()
