fac7c02854989b1ef7c87d3f6123083f9391e788
unknown <killian.adnot@gmail.com>
2020-03-04 23:25:44 +0100

    correction pour Antoine askip

--- sources/Terrain.lua
+++ sources/Terrain.lua
@@ -59,7 +59,6 @@ function Terrain:New(height, width) -- Générer une Terrain à  partir de 3 Til
 
     local next_perso = function()
         self.controlled_perso.setDestroying(false)
-        self.controlled_perso.changeWeapon("")
         self.teams[self.current_team_nb].setCurrentPlayer(self.next_perso_nb)
         self.teams[self.next_team_nb].reset_current_player()
         self.current_perso_index = self.teams[self.next_team_nb].getCurrentPlayer()
